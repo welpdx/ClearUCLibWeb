f959d065e5061868b5a18ff67322854638dade18
welpdx <30542416+welpdx@users.noreply.github.com>
2020-06-17 18:44:22 -0600

    Created code.js
    Javascript bookmark in Chrome

---
+++ code.js
@@ -0,0 +1,6 @@
+javascript: document.getElementsByClassName('lcr-alert')[0].remove(); const elements = document.getElementsByClassName("group"); while (elements.length > 0) elements[0].remove(); while (document.getElementsByClassName('row uc-logo-container')[0]) {        document.getElementsByClassName('sidebar')[0].remove(); };document.getElementsByClassName('row button')[0].remove();document.getElementsByClassName('s-lch-widget-float open')[0].remove(); void(0);
+
+// Ignore these comments
+// class 'lcr-alert' : covid-19 announcement
+// class 'group' : header stuff
+// class 's-lch-widget-float open' : chat box pop up
